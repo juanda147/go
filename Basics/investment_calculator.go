c36d8331ac65d21cbad473440ddc5247e0e9d629
Juan David Castaneda <juandacastaneda@gmail.com>
2024-11-28 07:28:56 -0500

    first function

--- Basics/investment_calculator.go
+++ Basics/investment_calculator.go
@@ -5,10 +5,12 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
+
 	var investmetAmount float64
-	years := 10.0
+	var years float64
 	expectedRerturnRate := 5.5
 
 	fmt.Print("Investment Amount: ")
@@ -20,11 +22,25 @@ func main() {
 	fmt.Print("Expected Return Rate: ")
 	fmt.Scan(&expectedRerturnRate)
 
-	futureValue := investmetAmount * math.Pow(1+expectedRerturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValue(investmetAmount, expectedRerturnRate, years)
+	// futureValue := investmetAmount * math.Pow(1+expectedRerturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
+	//formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	//formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.1f\n", futureRealValue)
 	//fmt.Println("Future Value: ", futureValue)
 	//fmt.Println("Future Value (adjusted for inflation): ", futureRealValue)
 	fmt.Printf("Future Value: %f\nFuture Value (adjusted for inflation): %f", futureValue, futureRealValue)
 
+	// fmt.Printf(`Future Value: %f\n
+	// Future Value (adjusted for inflation): %f`, futureValue, futureRealValue)
+
+	//fmt.Print(formattedFV, formattedRFV)
+
+}
+
+func calculateFutureValue(investmetAmount, expectedRerturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmetAmount * math.Pow(1+expectedRerturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
