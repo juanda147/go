9045545fa4c731adb015191d8a0e29187ea091a6
Juan David Castaneda <juandacastaneda@gmail.com>
2025-02-03 15:52:10 -0500

    interface and any parametrers - WIP

--- Structs-Practice/main.go
+++ Structs-Practice/main.go
@@ -5,28 +5,97 @@ import (
 	"fmt"
 	"os"
 	"strings"
-
-	"example.com/note/note"
 )
 
+type saver interface {
+	Save() error
+}
+
+// type displayer interface {
+// 	display()
+// }
+
+type outputtable interface {
+	saver
+	Display()
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
 func main() {
-	title, content := getNoteData()
+	// title, content := getNoteData()
+	// todoText := getUserInput("Todo text:")
 
-	userNote, err := note.New(title, content)
+	// todo, err := todo.New(todoText)
 
-	if err != nil {
-		fmt.Println((err))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	//userNote, err := note.New(title, content)
+
+	// if err != nil {
+	// 	fmt.Println((err))
+	// 	return
+	// }
+
+	// err = outputData(todo)
+
+	// if err != nil {
+	// 	return
+	// }
+
+	// err = outputData(userNote)
+}
+
+func getTodoData() string {
+	text := getUserInput("Todo text:")
+	return text
+}
+
+func printSomething(value any) {
+
+	typedVal, isInt := value.(int)
+
+	if isInt {
+		fmt.Println("Integer:", typedVal)
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+
+	// case float64:
+	// 	fmt.Println("Float:", value)
+
+	// case string:
+	// 	fmt.Println(value)
+
+	// 	//default:
+	// 	//...
+	// }
+}
+
+func outputData(data outputtable) {
+	data.Display()
+	saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
 
 	if err != nil {
 		fmt.Println("Saving the note failed.")
+		return err
 	}
 
 	fmt.Println("Saving the note succeded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
